55c03e15a64a73234f77fe9aa7d2d06168232b3a
MrWooJ <hurricanc@gmail.com>
2015-11-05 13:06:47 +0330

    Create Constants Class for All Hardcoded Texts and URLs

---
+++ src/com/lucene/LuceneConstants.java
@@ -0,0 +1,44 @@
+package com.lucene;
+
+public class LuceneConstants {
+
+	public static final String DATA_DIRECTORY 			= "/Users/Hurricanc/Documents/Projects/WJLucene-Server/MetaData/Data";
+	public static final String INDEX_DIRECTORY 			= "/Users/Hurricanc/Documents/Projects/WJLucene-Server/MetaData/Index";
+
+	public static final String DEFAULT_FIELD 			= "core_newsurl";
+	
+	public static final String CORE_NEWSURL 			= "core_news_url";
+	public static final String CORE_NEWSNUMBER 			= "core_news_number";
+	public static final String CORE_NEWSTITLE 			= "core_new_title";
+	public static final String CORE_NEWSBODY			= "core_news_body";
+	public static final String CORE_NEWSDATE			= "core_news_date";
+	public static final String CORE_NEWSSOURCE 			= "core_news_source";
+	
+	public static final String COMMENT_ID 				= "comment_id";
+	public static final String COMMENT_PARENTID 		= "comment_parentId";
+	public static final String COMMENT_COMMENTER 		= "comment_commenter";
+	public static final String COMMENT_LOCATION 		= "comment_location";
+	public static final String COMMENT_DATE 			= "comment_date";
+	public static final String COMMENT_LIKECOMMENT 		= "comment_like_comment";
+	public static final String COMMENT_DISLIKECOMMENT	= "comment_dislike_comment";
+	public static final String COMMENT_RESPONSECOUNT 	= "comment_response_count";
+	public static final String COMMENT_BODY 			= "comment_body";
+	
+	public static final String CATEGORY_NEWS 			= "core_category";
+	public static final String LABEL_NEWS 				= "core_label";
+	
+	public static final String FILE_NAME				= "filename";
+	public static final String FILE_PATH				= "filepath";
+	
+	public static final String FILE_PATH_CORENEWS 		= "/Users/Hurricanc/Documents/Projects/WJLucene-Server/MetaData/Data/Core.xls";
+	public static final String FILE_PATH_COMMENTS		= "/Users/Hurricanc/Documents/Projects/WJLucene-Server/MetaData/Data/Comments.xls";
+	public static final String FILE_PATH_CATEGORY		= "/Users/Hurricanc/Documents/Projects/WJLucene-Server/MetaData/Data/Category.xls";
+	public static final String FILE_PATH_LABELS			= "/Users/Hurricanc/Documents/Projects/WJLucene-Server/MetaData/Data/Label.xls";
+	
+	public static final int MAX_SEARCH = 50000;
+	
+	public static final String MESSAGE_INDEXING 		= "Wait for Merging raw data and Indexing!";
+	public static final String MESSAGE_INDEXED 			= "Ready to Use, Give it a try!";
+	public static final String MESSAGE_SEARCH 			= " Documents Found in ";
+
+}
